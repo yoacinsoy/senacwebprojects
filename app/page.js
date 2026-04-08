94b87257007107b04deb9b010c887ece4666fa01
Leopoldinoo <otavioleopoldino1@gmail.com>
2026-04-07 23:46:26 -0300

    fix

--- app/page.js
+++ app/page.js
@@ -1,9 +1,20 @@
 "use client";
 import Sidebar from "./components/Sidebar";
 import { use, useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
+
+  function enviarFormulario(e) {
+    e.preventDefault();
+    if (user === "admin" && password === "1234") {
+      router.push("/pageinicial");
+    }
+  }
+
   return (
     <div className="relative min-h-screen flex text-white font-sans overflow-hidden items-center justify-center">
       <div
@@ -22,6 +33,8 @@ export default function Home() {
               type="text"
               placeholder="Ex: admin"
               className="border border-gray-300 rounded py-2 text-black px-4 focus:outline-none focus:ring-2 focus:ring-gray-500"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
             />
           </div>
           <div className="grid gap-1">
@@ -30,9 +43,12 @@ export default function Home() {
               type="password"
               placeholder="Ex: 123456"
               className="border border-gray-300 rounded py-2 text-black px-4 focus:outline-none focus:ring-2 focus:ring-gray-500"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button
+            onClick={enviarFormulario}
             type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-gray-500"
           >
